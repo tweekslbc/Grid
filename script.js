2214e4da9bd32577b29aa4e2343fd4eb83681a11
Tweeks <tweeks@daiwa.com>
2019-07-28 15:54:42 -0700

    take two

---
+++ script.js
@@ -0,0 +1,18 @@
+<script>
+const rowsInput = document.querySelector("input[id = 'rows']");
+const columnsInput = document.quertySelector("input[id = 'columns']");
+const form = document.querySelector("form");
+const grid = document.querySelector("#gridContainer");
+form.addEventListener('submit', (ev)=> {
+  ev.preventDefault();
+  let html = "";
+  for (let i = 0; i < rowsInput.value; i++){
+    html += "<div class = 'cellStyle'>";
+    for (let j =0; j < columnsInput.value; j++){
+    html +=  "<div class = 'cellStyle'></div>";
+  }
+  html += "</div"
+  }
+  grid.innerHTML = html;
+})
+</script>
